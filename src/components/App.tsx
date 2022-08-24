0b601202549ebb5c1a83a2eec5a8fc526ce41414
Shehryar <shehryar.dev@gmail.com>
2022-08-24 16:28:08 +0400

    added log event

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,7 @@ import { error } from 'console';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, logEvent } from "firebase/analytics";
 import { firebaseConfig } from '../env/firebaseConfig';
 
 // Initialize Firebase
@@ -21,6 +21,7 @@ function App() {
   const [isNext, setIsNext] = useState(false);
 
   useEffect(() => {
+    logEvent(analytics, 'page_load');
     emailRef.current?.focus();
   }, []);
 
