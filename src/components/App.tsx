26d8454f486b88cbfeacf7f9f079a08f365f8fe0
Shehryar <shehryar.dev@gmail.com>
2022-08-24 12:13:51 +0400

    added firebase

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,27 +1,54 @@
+import { KeyboardEvent, useEffect, useRef, useState } from 'react';
 import '../styles/App.scss';
 
 function App() {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const [email, setEmail] = useState('');
+  const [isNext, setIsNext] = useState(false);
+
+  useEffect(() => {
+    emailRef.current?.focus();
+  }, []);
+
+  const handleKey = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      if (isNext) login();
+      else goNext();
+    }
+  }
+
+  const goNext = () => {
+    setIsNext(true);
+    setTimeout(() => passwordRef.current?.focus(), 500);
+  }
+
+  const login = () => {
+
+  }
+
   return (
     <div className="App">
       <div className="login_wrapper">
 
-        <div className="scroller email_scroller">
+        <div className={`scroller ${isNext ? "goLeft" : "comeCenter"}`}>
           <div className="input_wrapper">
-            <input type="email" name="email" id="email" />
+            <input type="email" name="email" id="email" value={email} onChange={e => setEmail(e.target.value)} onKeyDown={handleKey} ref={emailRef} />
             <label htmlFor="email" id="email_label">Email</label>
-            <button id="next">Next</button>
+            <button id="next" onClick={goNext}>Next</button>
+            <a href="/" id="forgot">Forgot Password?</a>
           </div>
         </div>
 
-        <div className="scroller password_scroller">
+        <div className={`scroller ${isNext ? "comeCenter" : "stayRight"}`}>
           <div className="input_wrapper">
-            <input type="email" name="email" id="email" />
-            <label htmlFor="email" id="email_label">Email</label>
-            <button id="next">Login</button>
+            <input type="password" name="password" id="password" onKeyDown={handleKey} ref={passwordRef} />
+            <label htmlFor="password" id="password_label">Passowrd</label>
+            <button id="login">Login</button>
+            <a href="/" id="forgot">Forgot Password?</a>
           </div>
         </div>
 
-        <a href="/" id="forgot">Forgot Password?</a>
       </div>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './components/App';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from './env/firebaseConfig';
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
